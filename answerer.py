e4302993daf07118f72441b2ec75402e0f4ecb15
Abhinandan <shtabhi@gmail.com>
2024-05-16 21:06:34 +0545

    Streamed Audio Data through Data Channel (Have to check possibility using track)

--- answerer.py
+++ answerer.py
@@ -7,17 +7,90 @@ import wave
 SIGNALING_SERVER_URL = 'http://localhost:9999'
 ID = "answerer01"
 
-async def save_audio_to_file(track):
-    audio_data = await track.recv()
+# Define a function to process the received message
+async def save_audio(message):
+
+    # Write the audio data to a WAV file
+    with wave.open('received-audio.wav', 'wb') as audio_file:
+        audio_file.setnchannels(2)
+        audio_file.setsampwidth(2)
+        audio_file.setframerate(44100)
+        audio_file.writeframes(message)
+
+
+async def main():
+    print("Starting")
+    
+    # Define STUN server configuration
+    stun_server = RTCIceServer(urls=["stun:stun.l.google.com:19302"])
+
+    # Create RTCConfiguration with STUN server
+    config = RTCConfiguration(iceServers=[stun_server])
+
+    peer_connection = RTCPeerConnection(configuration=config)
+
+    @peer_connection.on("datachannel")
+    def on_datachannel(channel):
+        print(channel, "-", "created by remote party")
+        channel.send("Hello From Answerer via RTC Datachannel")
+
+        @channel.on("message")
+        async def on_message(message):
+            print("Received via RTC Datachannel: ", message)
+            await save_audio(message)
+
+    # @peer_connection.on("track")
+    # def on_track(track):
+    #     if track.kind == "audio":
+    #         print("Received audio track")
+    #         audio_data = track.recv()
+    #         print(dir(audio_data))
+    #         # asyncio.ensure_future(save_audio_to_file(track))
+
+    resp = requests.get(SIGNALING_SERVER_URL + "/get_offer")
+
+    print(resp.status_code)
+    # print(resp.json())
+    if resp.status_code == 200:
+        data = resp.json()
+        if data["type"] == "offer":
+            rd = RTCSessionDescription(sdp=data["sdp"], type=data["type"])
+            await peer_connection.setRemoteDescription(rd)
+            await peer_connection.setLocalDescription(await peer_connection.createAnswer())
+
+            message = {"id": ID, "sdp": peer_connection.localDescription.sdp, "type": peer_connection.localDescription.type}
+            r = requests.post(SIGNALING_SERVER_URL + '/answer', data=message)
+            print(message)
+            while True:
+                print("Ready for Stuff")
+                await asyncio.sleep(1)
+
+asyncio.run(main())
+
+
+# async def save_audio_to_file(track):
+#     print(dir(track))
+#     audio_data = await track.recv()
+#     print(audio_data)
+
     # print(audio_data)
+    # print(audio_data.format.name)
+    # print(len(audio_data.to_ndarray()[0]))
+
     # print(dir(audio_data))
-    print(audio_data.pts)
-    print(audio_data.from_ndarray)
-    print(len(audio_data.layout.channels))
-    print(audio_data.rate)
-    print(audio_data.sample_rate)
-    print(audio_data.samples)
-    print(audio_data.to_ndarray)
+
+    # # Convert the AudioFrame to a numpy array
+    # audio_array = audio_data.to_ndarray()
+
+    # print(audio_data)
+    # print(audio_data.is_corrupt)
+    # # print(audio_data.pts)
+    # print(audio_data.from_ndarray())
+    # print(len(audio_data.layout.channels))
+    # print(audio_data.rate)
+    # print(audio_data.sample_rate)
+    # print(audio_data.samples)
+    # print(audio_data.to_ndarray)
 
 # async def save_audio_to_file(track, peer_id):
 #     try:
@@ -39,41 +112,3 @@ async def save_audio_to_file(track):
 #             print("Successfully saved audio to output.wav")
 #     except Exception as e:
 #         print(f"Error saving audio: {e}")
-
-async def main():
-    print("Starting")
-    
-    # Define STUN server configuration
-    stun_server = RTCIceServer(urls=["stun:stun.l.google.com:19302"])
-
-    # Create RTCConfiguration with STUN server
-    config = RTCConfiguration(iceServers=[stun_server])
-
-    peer_connection = RTCPeerConnection(configuration=config)
-
-
-    @peer_connection.on("track")
-    def on_track(track):
-        if track.kind == "audio":
-            print("Received audio track")
-            asyncio.ensure_future(save_audio_to_file(track))
-
-    resp = requests.get(SIGNALING_SERVER_URL + "/get_offer")
-
-    print(resp.status_code)
-    # print(resp.json())
-    if resp.status_code == 200:
-        data = resp.json()
-        if data["type"] == "offer":
-            rd = RTCSessionDescription(sdp=data["sdp"], type=data["type"])
-            await peer_connection.setRemoteDescription(rd)
-            await peer_connection.setLocalDescription(await peer_connection.createAnswer())
-
-            message = {"id": ID, "sdp": peer_connection.localDescription.sdp, "type": peer_connection.localDescription.type}
-            r = requests.post(SIGNALING_SERVER_URL + '/answer', data=message)
-            print(message)
-            while True:
-                print("Ready for Stuff")
-                await asyncio.sleep(1)
-
-asyncio.run(main())
--- offerer.py
+++ offerer.py
@@ -1,12 +1,27 @@
 from aiortc import RTCIceCandidate, RTCPeerConnection, RTCSessionDescription, RTCConfiguration, RTCIceServer
-import json
 import asyncio
-import requests, aiohttp
-import recorder
-from aiortc.contrib.media import MediaPlayer, MediaStreamTrack
+import requests
+from aiortc.contrib.media import  MediaStreamTrack
+import pyaudio
 
 SIGNALING_SERVER_URL = 'http://localhost:9999'
 ID = "offerer01"
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 5
+
+class MicrophoneAudioStream(MediaStreamTrack):
+    kind = "audio"
+
+    def __init__(self, audio_buffer):
+        super().__init__()
+        self.audio_buffer = audio_buffer
+
+    async def recv(self):
+        yield audio_buffer
 
 async def main():
     print("Starting")
@@ -18,10 +33,43 @@ async def main():
     config = RTCConfiguration(iceServers=[stun_server])
 
     peer_connection = RTCPeerConnection(configuration=config)
+    channel = peer_connection.createDataChannel("audio")
 
-    player = MediaPlayer('sample.wav')
-    print(player.audio)
-    peer_connection.addTrack(player.audio)
+    audio_buffer=b''
+
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("* recording")
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        audio_buffer += data
+
+    print("* done recording")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    print("Starting streaming...")
+
+    @channel.on("open")
+    def on_open():
+        print("channel openned")
+        channel.send(audio_buffer)
+
+    @channel.on("message")
+    def on_message(message):
+        print("Received via RTC Datachannel", message)
+
+    # # Add recorded audio as a track to the peer connection
+    # audio_track = MicrophoneAudioStream(audio_buffer)
+    # audio_track = peer_connection.addTrack(audio_track)
 
     # send offer
     await peer_connection.setLocalDescription(await peer_connection.createOffer())
